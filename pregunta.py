a52005a95455d0926036bf1f8618265e8ffd7bb0
jomorenoo <jomorenoo@unal.edu.co>
2024-03-12 12:40:36 -0500

    intento1

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,32 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
 
 
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df.dropna(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
+    # convierto todas las columnas a cadenas para manipular
+    df = df.apply(lambda x: x.astype(str))
+
+    # limpio caracteres no deseados
+    df = df.apply(lambda x: x.str.replace("$", ""))
+    df = df.apply(lambda x: x.str.replace(",", ""))
+    df = df.apply(lambda x: x.str.replace("_", ""))
+    df = df.apply(lambda x: x.str.lower())
+
+    # convierto el monto del credito a float
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+
+    # convierto fecha a formano año-mes-dia
+    df.fecha_de_beneficio = pd.to_datatime(
+        df["fecha_de_beneficio"], dayfirst=True, format="mixed"
+    )
+
+    df.drop_duplicates(inplace=True)
 
     return df
